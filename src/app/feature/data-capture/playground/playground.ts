b23de52b5ec30df446cad913bc54ae5adbd68367
peggylicious <p.asq@live.com>
2026-02-24 14:15:58 +0100

    feat(playground): player event entry Updates and fixes - Add Functionality to populate matchLogEntry (a collection of selected player events and outcomes) array that will be used by table - Update pitch styling to improve pitch height responsiveness

--- src/app/feature/data-capture/playground/playground.ts
+++ src/app/feature/data-capture/playground/playground.ts
@@ -5,15 +5,18 @@ import {PlayerEvents} from '../../../ui/player-events/player-events';
 import {footballEventsGoogleSet} from '../constants/player-events';
 import {PlayerCard} from '../../../ui/player-card/player-card';
 import {matchDayConfig} from '../constants/match';
-import {Player} from '../interfaces/player';
+import {MatchEventLogEntry, Player} from '../interfaces/player';
 import {MatchConfig, Team, TeamSide} from '../interfaces/team';
+import {DataTable} from '../../../ui/data-table/data-table';
+import {EventOutcome, FootballItem} from '../interfaces/player-events';
 
 @Component({
   selector: 'app-playground',
   imports: [
     Pitch,
     PlayerEvents,
-    PlayerCard
+    PlayerCard,
+    DataTable
   ],
   templateUrl: './playground.html',
   styleUrl: './playground.scss'
@@ -22,7 +25,7 @@ export class Playground {
   public PLAYER_EVENTS = signal(footballEventsGoogleSet)
   public MATCHDAY_CONFIG = signal(matchDayConfig)
   public query = signal('')
-
+  public selectedPlayerData = signal<Partial<MatchEventLogEntry>>({})
   public matchLogEntry = signal<MatchEventLogEntry[]>([])
   filteredPlayersList = computed(()=> {
     const searchTerm = this.query().toLowerCase();
@@ -41,21 +44,37 @@ export class Playground {
     }
   })
 
-  updatePlayerEntry($event: MatchingZone) {
-    console.log('updatePlayerEntry', $event);
+  updatePlayerEntry(zone: MatchingZone) {
+    this.selectedPlayerData.update(state => {
+      return {
+        ...state,
+        coordinates: {
+          ...state.coordinates,
+          x: zone.x,
+          y: zone.y
+        }
+      }
+    })
   }
 
   selectPlayer(selectedPlayer: Player, teamSide: TeamSide) {
     this.MATCHDAY_CONFIG.update(state => {
       return {
         ...state,
-        home: this.updateSelectedPlayer(selectedPlayer, state.home, teamSide === 'home'),
-        away: this.updateSelectedPlayer(selectedPlayer, state.away, teamSide === 'away')
+        home: this.updateSelectedPlayerStatus(selectedPlayer, state.home, teamSide === 'home'),
+        away: this.updateSelectedPlayerStatus(selectedPlayer, state.away, teamSide === 'away')
+      }
+    })
+    this.selectedPlayerData.update(state => {
+      return {
+        ...state,
+        playerId: selectedPlayer.id,
+        playerName: selectedPlayer.name,
       }
     })
   }
 
-  updateSelectedPlayer(selectedPlayer: Player, team: Team, isTargetTeam: boolean){
+  updateSelectedPlayerStatus(selectedPlayer: Player, team: Team, isTargetTeam: boolean){
     const updatedPlayersList =  team.players.map((player) => {
       return {
         ...player,
@@ -66,5 +85,23 @@ export class Playground {
       ...team,
       players: updatedPlayersList
     }
+  }
+
+  updatePlayerEvent(data: { item: FootballItem; outcome: EventOutcome }) {
+    this.selectedPlayerData.update(state => {
+      const {item, outcome} = data;
+      return {
+        ...state,
+        outcome: outcome,
+        eventTypeId: item.id,
+        eventTypeName: item.name,
+        categoryType: item.categoryType,
+        timeStamp: new Date()
+      }
+    })
+    this.matchLogEntry.update(state => {
+      return[...state, this.selectedPlayerData()  as MatchEventLogEntry];
+    })
+    this.selectedPlayerData.set({})
   }
 }
